c57a1e4e5fffaf0007ecd454f3d0777f682a73c2
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-01 12:20:40 +0100

    Move handleClick from submit button to onSubmit method on the form. Use event.preventDefault() method in handleClick function to prevent from from refreshing after submit button is clicked.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,35 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 function App() {
 
-  const [name, setName] = useState("");
-  const [heading, setHeading] = useState("");
+    const [name,
+        setName] = useState("");
+    const [heading,
+        setHeading] = useState("");
 
-  function handleChange(event) {
-    setName(event.target.value);
-  }
+    function handleChange(event) {
+        setName(event.target.value);
+    }
 
-  function handleClick() {
-    setHeading(name);
-  }
+    function handleClick(event) {
+        setHeading(name);
+        event.preventDefault();
+    }
 
-
-  return (
-    <div className="container">
-      <h1>Hello  {heading} </h1>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" value={name} />
-      <button onClick={handleClick}>Submit</button>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Hello {heading}
+            </h1>
+            <form onSubmit={handleClick}>
+                <input
+                    onChange={handleChange}
+                    type="text"
+                    placeholder="What's your name?"
+                    value={name}/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
